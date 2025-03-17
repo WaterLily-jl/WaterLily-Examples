02742ac4d5c30febd2d596ffd22ba02f328e5df6
marinlauber <marinlauber@tudelft.nl>
2025-03-17 18:09:30 +0100

    fix TwoD_Channel.jl

--- examples/TwoD_Channel.jl
+++ examples/TwoD_Channel.jl
@@ -21,16 +21,16 @@ function BC_CH!(a,perdir=())
         end
     end
 end
-@fastmath function WaterLily.mom_step!(a::Flow{N},b::AbstractPoisson) where N
-    a.u⁰ .= a.u; WaterLily.scale_u!(a,0)
+@fastmath function WaterLily.mom_step!(a::Flow{N},b::AbstractPoisson;udf=nothing,kwargs...) where N
+    a.u⁰ .= a.u; WaterLily.scale_u!(a,0); t₁ = sum(a.Δt); t₀ = t₁ - a.Δt[end]
     # predictor u → u'
     WaterLily.conv_diff!(a.f,a.u⁰,a.σ,ν=a.ν,perdir=a.perdir)
-    WaterLily.accelerate!(a.f,@view(a.Δt[1:end-1]),a.g,a.U)
+    WaterLily.accelerate!(a.f,t₀,a.g,a.U)
     WaterLily.BDIM!(a); BC_CH!(a.u,a.perdir)
     WaterLily.project!(a,b); BC_CH!(a.u,a.perdir)
     # corrector u → u¹
     WaterLily.conv_diff!(a.f,a.u,a.σ,ν=a.ν,perdir=a.perdir)
-    WaterLily.accelerate!(a.f,@view(a.Δt[1:end-1]),a.g,a.U)
+    WaterLily.accelerate!(a.f,t₁,a.g,a.U)
     WaterLily.BDIM!(a); WaterLily.scale_u!(a,0.5); BC_CH!(a.u,a.perdir)
     WaterLily.project!(a,b,0.5); BC_CH!(a.u,a.perdir)
     push!(a.Δt,WaterLily.CFL(a))
@@ -39,7 +39,7 @@ end
 function channel(;L=2^6,U=1.0,Re=100,T=Float32,mem=Array)
 
     # pressure gradient required to drive the flow to u~1
-    g(i,x,t) = i == 1 ? U^2/(L/2)^2 : 0
+    g(i,x,t) = convert(typeof(t), i == 1 ? U^2/(L/2)^2 : 0)
 
     # make the simulations
     sim = Simulation((4L,L),(0,0),L;U=U,ν=U*L/Re,g,perdir=(1,),T,mem)
@@ -49,12 +49,10 @@ end
 # make the sim
 sim = channel(L=2^7)#;mem=CUDA.CuArray
 
-
 # get start time
-t₀ = round(sim_time(sim))
 duration = 50; step = 0.1
 
-anim = @animate for tᵢ in range(t₀,t₀+duration;step)
+@gif for tᵢ in range(0,duration;step)
 
     # update until time tᵢ in the background
     sim_step!(sim,tᵢ)
@@ -66,11 +64,10 @@ anim = @animate for tᵢ in range(t₀,t₀+duration;step)
     # print time step
     println("tU/L=",round(tᵢ,digits=4),", Δt=",round(sim.flow.Δt[end],digits=3))
 end
-gif(anim,"chanel_flow.gif",fps=15)
 
-# # plot the profile
-# plot(sim.flow.u[:,2:end,1]'./maximum(sim.flow.u[:,:,1]),collect(1:129)./129,
-#     label=:none,xlabel="u/U",ylabel="y/L")
+# plot the profile
+# plot(sim.flow.u[:,2:end,1]'./maximum(sim.flow.u[:,:,1]),collect(1:33)./33,
+#      label=:none,xlabel="u/U",ylabel="y/L")
 # # analytical solution laminar chanel flow u/U ~ C*(y/L)-(y/L)^2 ∀ y ∈ [0,L/2]
 # plot!(4*(collect(0:0.01:0.5).-collect(0:0.01:0.5).^2),2collect(0:0.01:0.5),label="analytical")
 # savefig("chanel_flow.png")
