5259d710e79cf7b6332df099492757779e0acbd9
Gabriel Weymouth <drweymouth@google.com>
2025-06-15 18:57:28 +0200

    Update TwoD_MultipleBodies.jl

--- examples/TwoD_MultipleBodies.jl
+++ examples/TwoD_MultipleBodies.jl
@@ -7,11 +7,11 @@ function circle(n,m;Re=550,U=1,mem=Array,T=Float32)
         AutoBody((x,t)->√sum(abs2, x .- x0 .- 2center.*R) - radius*R)
     end
     # make a simulation
-    Simulation((n,m), (U,0), R; ν=U*R/Re, body, mem, T),bodies
+    Simulation((n,m), (U,0), R; ν=U*R/Re, body, mem, T)
 end
 
 # make a simulation and run it
-sim,bodies = circle(3*2^7,2^8,mem=Array);
+sim = circle(3*2^7,2^8,mem=Array);
 sim_gif!(sim,duration=1,clims=(-5,5),remeasure=false,plotbody=true,axis=([], false),
          cfill=:seismic,legend=false,border=:none)
 
@@ -19,11 +19,6 @@ sim_gif!(sim,duration=1,clims=(-5,5),remeasure=false,plotbody=true,axis=([], fal
 f_total = WaterLily.pressure_force(sim)
 
 # force on _each_ body
-each_force(sim,body) = WaterLily.pressure_force(sim)
-each_force(sim,body::SetBody) = WaterLily.pressure_force(sim)
-
-f_each = mapreduce(body->WaterLily.pressure_force(sim.flow,body) for body in bodies]
-
-# check that this is only non-zero near a single body
-WaterLily.pressure_force(sim.flow,bodies[1]);
-flood(sim.flow.f[:,:,1],clims=(-1,1))
+each_force(flow,body) = WaterLily.pressure_force(flow,body)
+each_force(flow,body::WaterLily.SetBody) = mapreduce(bod->each_force(flow,bod),hcat,(body.a,body.b))
+each_force(sim.flow,sim.body)
