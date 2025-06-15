65f1f02b84ca46caf35d0427042952d7ac49d437
Gabriel Weymouth <drweymouth@google.com>
2025-06-15 20:46:36 +0200

    Update MultipleAbstractBodies

--- examples/TwoD_MultiplesAbstractBodies.jl
+++ examples/TwoD_MultiplesAbstractBodies.jl
@@ -1,33 +1,19 @@
-using WaterLily
-using StaticArrays
-using ParametricBodies
-include("../src/TwoD_plots.jl")
+using WaterLily, StaticArrays, ParametricBodies, Plots
 
-function circle(L;Re=550,U=1,mem=Array,T=Float32)
-    bodies = AbstractBody[]
-    # a first circle using autoBody 
-    push!(bodies,AutoBody((x,t)->√sum(abs2, x .- SA_F32[3.75L,2.5L]) - L/2))
-    # another one from parametricbodies
-    cps = SA_F32[1 1 0 -1 -1 -1  0  1 1
-                0 1 1  1  0 -1 -1 -1 0]*L/2 .+ [2L,1.5L]
-    weights = SA_F32[1.,√2/2,1.,√2/2,1.,√2/2,1.,√2/2,1.]
-    knots =   SA_F32[0,0,0,1/4,1/4,1/2,1/2,3/4,3/4,1,1,1]
-    # make a nurbs curve and a body for the simulation
-    push!(bodies,ParametricBody(NurbsCurve(cps,knots,weights)))
-    # combine into one body
-    body = Bodies(bodies, [+])
-    # make a simulation
-    Simulation((10L,4L), (U,0), L/2; ν=U*L/Re, body, mem, T)
+function circle_and_foil(L;Re=550,U=1,mem=Array,T=Float32)
+    # A moving circle using AutoBody
+    circle = AutoBody((x,t)->√sum(abs2,x)-L÷2, (x,t)->x-SA[L+U*t,3L÷2-10])
+
+    # A foil defined by the difference between an upper and lower surface
+    upper = ParametricBody(BSplineCurve(SA[5L 4L 3L 3L; 2L 5L÷2 5L÷2 2L],degree=3;T))
+    lower = ParametricBody(BSplineCurve(SA[5L 3L; 2L 2L],degree=1;T))
+    foil = upper-lower
+
+    # Simulate the flow past the two general bodies
+    Simulation((10L,4L), (U,0), L; ν=U*L/Re, body=foil+circle, mem, T)
 end
 
 # make a simulation and run it
-sim = circle(2^6,mem=Array)
-sim_gif!(sim,duration=30,clims=(-5,5),plotbody=true,axis=([], false),
+sim = circle_and_foil(2^6);
+sim_gif!(sim,duration=8,clims=(-5,5),remeasure=true,plotbody=true,axis=([], false),
          cfill=:seismic,legend=false,border=:none)
-
-# # get force on first body
-# f1 = WaterLily.pressure_force(sim.flow,sim.body.bodies[1])
-# flood(sim.flow.f[:,:,1],clims=(-1,1)) # check that this is only non-zero near the first body
-
-# # force on the second
-# f2 = WaterLily.pressure_force(sim.flow,sim.body.bodies[2])
