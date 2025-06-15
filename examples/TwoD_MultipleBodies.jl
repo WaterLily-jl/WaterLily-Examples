0c04501f87a9eed34c82ee246bfd56b6afe783ad
Gabriel Weymouth <drweymouth@google.com>
2025-06-15 17:40:37 +0200

    Update TwoD_MultipleBodies.jl

--- examples/TwoD_MultipleBodies.jl
+++ examples/TwoD_MultipleBodies.jl
@@ -1,29 +1,29 @@
-using WaterLily
-using StaticArrays
-include("../src/TwoD_plots.jl")
+using WaterLily,StaticArrays,Plots
 
 function circle(n,m;Re=550,U=1,mem=Array,T=Float32)
     R, x0 = m/18, m/2+1
-    bodies = AbstractBody[]
     # random position x,y ∈ [-2.5,2.5] and circle diamater r ∈ [0.75,1.5]
-    for (center,radius) ∈ zip(eachrow(5rand(6,2).-2.5),0.75rand(6).+0.75)
-        push!(bodies,AutoBody((x,t)->√sum(abs2, x .- x0 .- 2center.*R) - radius*R))
+    body = sum(zip(eachrow(5rand(6,2).-2.5),0.75rand(6).+0.75)) do (center,radius)
+        AutoBody((x,t)->√sum(abs2, x .- x0 .- 2center.*R) - radius*R)
     end
-    # combine into one body
-    body = Bodies(bodies, repeat([+],length(bodies)-1))
     # make a simulation
-    Simulation((n,m), (U,0), R; ν=U*R/Re, body, mem, T)
+    Simulation((n,m), (U,0), R; ν=U*R/Re, body, mem, T),bodies
 end
 
 # make a simulation and run it
-sim = circle(3*2^7,2^8,mem=Array)
-sim_gif!(sim,duration=30,clims=(-5,5),plotbody=true,axis=([], false),
+sim,bodies = circle(3*2^7,2^8,mem=Array);
+sim_gif!(sim,duration=1,clims=(-5,5),remeasure=false,plotbody=true,axis=([], false),
          cfill=:seismic,legend=false,border=:none)
 
-# get force on first body
-f1 = WaterLily.pressure_force(sim.flow,sim.body.bodies[1])
-flood(sim.flow.f[:,:,1],clims=(-1,1)) # check that this is only non-zero near the first body
+# get net force on all bodies
+f_total = WaterLily.pressure_force(sim)
 
-# force on the second
-f2 = WaterLily.pressure_force(sim.flow,sim.body.bodies[2])
-flood(sim.flow.f[:,:,1],clims=(-1,1)) # check that this is only non-zero near the first body
+# force on _each_ body
+each_force(sim,body) = WaterLily.pressure_force(sim)
+each_force(sim,body::SetBody) = WaterLily.pressure_force(sim)
+
+f_each = mapreduce(body->WaterLily.pressure_force(sim.flow,body) for body in bodies]
+
+# check that this is only non-zero near a single body
+WaterLily.pressure_force(sim.flow,bodies[1]);
+flood(sim.flow.f[:,:,1],clims=(-1,1))
