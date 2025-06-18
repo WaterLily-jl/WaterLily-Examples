5e7e03e8d115bd19f1017ec613acc962eccb75df
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2025-06-18 10:42:35 +0200

    Update circle and foil example to use viz!

--- examples/TwoD_MultiplesAbstractBodies.jl
+++ examples/TwoD_MultiplesAbstractBodies.jl
@@ -1,10 +1,10 @@
-using WaterLily, StaticArrays, ParametricBodies, Plots
+using WaterLily, StaticArrays, ParametricBodies, GLMakie, CUDA
 
-function circle_and_foil(L;Re=550,U=1,mem=Array,T=Float32)
+function circle_and_foil(L=2^6;Re=550,U=1,mem=CUDA.CuArray,T=Float32)
     # A moving circle using AutoBody
     circle = AutoBody((x,t)->√sum(abs2,x)-L÷2, (x,t)->x-SA[L+U*t,3L÷2-10])
 
-    # A foil defined by the difference between an upper and lower surface
+    # A foil defined by the intersection of the upper and lower surface
     upper = ParametricBody(BSplineCurve(SA{T}[5L 4L 3L 3L; 2L 5L÷2 5L÷2 2L],degree=3))
     lower = ParametricBody(BSplineCurve(SA{T}[3L 5L; 2L 2L],degree=1))
     foil = upper ∩ lower
@@ -14,7 +14,4 @@ function circle_and_foil(L;Re=550,U=1,mem=Array,T=Float32)
 end
 
 # make a simulation and run it
-#using CUDA
-sim = circle_and_foil(2^6);#mem=CUDA.CuArray);
-sim_gif!(sim,duration=8,clims=(-5,5),remeasure=true,plotbody=true,axis=([], false),
-         cfill=:seismic,legend=false,border=:none)
+viz!(circle_and_foil(),duration=9,clims=(-0.1,0.1))
