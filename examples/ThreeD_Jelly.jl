07ae558985a07f7cedfe87faf364646da0416bd2
marinlauber <marinlauber@gmail.com>
2025-09-09 17:10:39 +0200

    fix jelly on GPU

--- examples/ThreeD_Jelly.jl
+++ examples/ThreeD_Jelly.jl
@@ -2,18 +2,17 @@ using WaterLily,StaticArrays,GLMakie
 
 function jelly(L=2^5;Re=5e2,mem=Array,U=1,T=Float32)
     # Define simulation size, geometry dimensions, & viscosity
-    R = 2L/3; h = 4L-2R; ν = U*R/Re
-
+    R = T(2L/3); h = T(4L-2R); ν = U*R/Re
     # Motion functions
-    ω = 2U/R
-    @fastmath @inline A(t) = 1 .- SA[1,1,0]*0.1*cos(ω*t)
+    ω = T(2U/R)
+    @fastmath @inline A(t) = 1 .- SA[1,1,0]*cos(ω*t)/10
     @fastmath @inline B(t) = SA[0,0,1]*((cos(ω*t)-1)*R/4-h)
     @fastmath @inline C(t) = SA[0,0,1]*sin(ω*t)*R/4
 
     # Build jelly from a mapped sphere and plane
     sphere = AutoBody((x,t)->abs(√sum(abs2,x)-R)-1, # sdf
                       (x,t)->A(t).*x+B(t)+C(t))     # map
-    plane = AutoBody((x,t)->x[3]-h,(x,t)->x+C(t))
+    plane = AutoBody((x,t)->x[3]-h,(x,t)->x .+ C(t))
     body =  sphere-plane
 
     # Return initialized simulation
@@ -28,7 +27,7 @@ end
 
 # using CUDA
 # make sim and run
-sim = jelly(2^5)#;mem=CuArray);
+sim = jelly(2^5)#; mem=CuArray)
 t₀ = sim_time(sim)
 duration = 5.0
 step = 0.1
