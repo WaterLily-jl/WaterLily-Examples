4a861cc367b3c40dafb548b2616b2026be690b43
michaelkonshin <mkonshin0388@sdsu.edu>
2026-04-14 23:57:14 -0700

    Initialized the file containing the wing example

---
+++ examples/ThreeD_AsymmetricalWing.jl
@@ -0,0 +1,8 @@
+# Preliminary code initialization
+using WaterLily, GLMakie, StaticArrays, ParametricBodies
+
+# define a function for the wing
+
+# define the parameters of the simulation
+
+# run the solver and visual the results
